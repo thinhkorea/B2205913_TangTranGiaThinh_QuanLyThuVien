9ec4e1e17a8dd47f32fb6ca39e5a01f6e5d62c8a
Tang Tran Gia Thinh <giathinh3503@gmail.com>
2025-11-02 13:00:36 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from MEVN backend!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
